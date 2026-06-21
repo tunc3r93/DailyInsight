20974250eddd2e4bb2dab5269b8f12d5c176ace2
tunc3r93 <178416938+tunc3r93@users.noreply.github.com>
2026-06-21 15:04:19 +0200

    feat: new Model Class for NavItems

---
+++ DailyInsight/Models/NavItem.cs
@@ -0,0 +1,9 @@
+namespace DailyInsight.Models;
+
+public class NavItem
+{
+    public int Id { get; set; }
+    public string Label { get; set; } = "";
+    public string IconName { get; set; } = "";
+    public string IconClass { get; set; } = "material-icons";
+}
